c7b5a271da4457b03849425a426eb0280e061550
Boi Soth <cambodianjesuss@gmail.com>
2023-01-04 20:15:54 -0800

    Unsplash API set up for fetching data

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-// Begin
-console.log("Testing")
+const count = 10;
+const apiKey = 'KL1ne9E6KAmbf6N48hUihxOyL6a8c4XWf0OBMkHgXbw';
+// Unsplash API
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get Photos from Unsplash API
+const getPhotos = async () => {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch(error){
+    console.log(error);
+  }
+}
+
+// On Load
+getPhotos();
