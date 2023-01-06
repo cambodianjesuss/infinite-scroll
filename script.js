dc98063a3bbe2041260831c30370aeedd925f468
Boi Soth <cambodianjesuss@gmail.com>
2023-01-05 22:56:39 -0800

    refactoring attributes and creating helper function

--- script.js
+++ script.js
@@ -8,17 +8,28 @@ const count = 10;
 const apiKey = 'KL1ne9E6KAmbf6N48hUihxOyL6a8c4XWf0OBMkHgXbw';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Helper Function Setting Attributes
+const setAttributes = (element, attributes) => {
+  for(const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
 // Create Elements for Links & Photos, and Add to DOM
 const displayPhotos = () =>{
   photosArray.forEach((photo)=>{
     const item = document.createElement('a');
-    item.setAttribute('href', photo.links.html);
-    item.setAttribute('target', '_blank');
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
 
     const img = document.createElement('img');
-    img.setAttribute('src', photo.urls.regular);
-    img.setAttribute('alt', photo.alt_description);
-    img.setAttribute('title', photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description
+    });
 
     item.appendChild(img);
     imageContainer.appendChild(item);
